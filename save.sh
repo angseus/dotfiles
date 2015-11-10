1246c25180b61b114b313419332bf099068d000d
Johan Angséus <angseus@student.chalmers.se>
2015-11-10 20:15:17 +0100

    Added bash scripts

---
+++ save.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/
+cp .gitconfig ~/develop/dotfiles
+cp -r .oh-my-zsh/ ~/develop/dotfiles
+cp -r .vim/ ~/develop/dotfiles
+cp -r .viminfo ~/develop/dotfiles
+cp -r .vimrc ~/develop/dotfiles
+cp -r .zshrc ~/develop/dotfiles
