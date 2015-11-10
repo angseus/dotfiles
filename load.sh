84cb4341aadc41a42c3b2b31b29c575c5fc38b7e
Johan Angséus <angseus@student.chalmers.se>
2015-11-10 20:43:19 +0100

    pew

--- load.sh
+++ load.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ~/develop/dotfiles
-tar -czf dotfiles.tar.gz .gitconfig .vim/ .viminfo .vimrc .zshrc .oh-my-zsh/
-mv dotfiles.tar.gz ~/
+#cd ~/develop/dotfiles
+#tar -czf dotfiles.tar.gz .gitconfig .vim/ .viminfo .vimrc .zshrc .oh-my-zsh/
+cp dotfiles.tar.gz ~/
 cd ~/
 tar -zxvf dotfiles.tar.gz
 rm dotfiles.tar.gz
--- save.sh
+++ save.sh
@@ -3,6 +3,6 @@
 cd ~/
 tar -czf dotfiles.tar.gz .gitconfig .vim/ .viminfo .vimrc .zshrc .oh-my-zsh/
 mv dotfiles.tar.gz ~/develop/dotfiles
-cd ~/develop/dotfiles
-tar -zxvf dotfiles.tar.gz
-rm dotfiles.tar.gz
+#cd ~/develop/dotfiles
+#tar -zxvf dotfiles.tar.gz
+#rm dotfiles.tar.gz
