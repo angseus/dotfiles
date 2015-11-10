46d8536e704236fb44b8a255eb81bb0b377b9314
Johan Angséus <angseus@student.chalmers.se>
2015-11-10 20:37:25 +0100

    load should work now

--- load.sh
+++ load.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cp -R .* ~/
+cd ~/develop/dotfiles
+tar -czf dotfiles.tar.gz .gitconfig .vim/ .viminfo .vimrc .zshrc .oh-my-zsh/
+mv dotfiles.tar.gz ~/
+cd ~/
+tar -zxvf dotfiles.tar.gz
+rm dotfiles.tar.gz
